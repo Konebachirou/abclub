fb20ca170eb54d3244bc4160d9291fa65fc8c6e2
konebachirou <konebachirou1@gmail.com>
2023-09-17 10:15:31 +0200

    favicon

--- resources/views/users/global/home.blade.php
+++ resources/views/users/global/home.blade.php
@@ -498,46 +498,47 @@
         </div>
     </div>
 
-    <!-- Start Brand Area  -->
-    <div class="rbt-brand-area bg-color-secondary-alt rbt-section-gap">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-12">
-                    <div class="section-title text-center mb--40">
-                        <h4 class="small-title w-600">Ils nous font confiance</h4>
+        <!-- Start Brand Area  -->
+        <div class="rbt-brand-area bg-color-secondary-alt" style="padding-top: 5%; padding-bottom: 5%;">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-12">
+                        <div class="section-title text-center mb--40">
+                            <h4 class="small-title w-600">Ils nous font confiance</h4>
+                        </div>
+                        <ul class="brand-list brand-style-3 justify-content-center justify-content-lg-between">
+                            <li><a href="#"><img src="{{ asset('assets/images/partners/part3.png') }}"
+                                        alt="Brand Image"></a></li>
+                            <li><a href="#"><img src="{{ asset('assets/images/partners/part1.png') }}"
+                                        alt="Brand Image"></a></li>
+                            <li><a href="#"><img src="{{ asset('assets/images/partners/part2.png') }}"
+                                        alt="Brand Image"></a></li>
+
+                            <li><a href="#"><img src="{{ asset('assets/images/partners/part4.png') }}"
+                                        alt="Brand Image"></a></li>
+                            <li><a href="#"><img src="{{ asset('assets/images/partners/part5.png') }}"
+                                        alt="Brand Image"></a></li>
+                        </ul>
+                        <ul class="brand-list brand-style-3 justify-content-center justify-content-lg-between">
+                            <li><a href="#"><img src="{{ asset('assets/images/partners/part6.png') }}"
+                                        alt="Brand Image"></a></li>
+                            <li><a href="#"><img src="{{ asset('assets/images/partners/part7.png') }}"
+                                        alt="Brand Image"></a></li>
+                            <li><a href="#"><img src="{{ asset('assets/images/partners/part8.png') }}"
+                                        alt="Brand Image"></a></li>
+                            <li><a href="#"><img src="{{ asset('assets/images/partners/part9.png') }}"
+                                        alt="Brand Image"></a></li>
+                            <li><a href="#"><img src="{{ asset('assets/images/partners/part10.png') }}"
+                                        alt="Brand Image"></a></li>
+                        </ul>
                     </div>
-                    <ul class="brand-list brand-style-3 justify-content-center justify-content-lg-between">
-                        <li><a href="#"><img src="{{ asset('assets/images/partners/part1.png') }}"
-                                    alt="Brand Image"></a></li>
-                        <li><a href="#"><img src="{{ asset('assets/images/partners/part2.png') }}"
-                                    alt="Brand Image"></a></li>
-                        <li><a href="#"><img src="{{ asset('assets/images/partners/part3.png') }}"
-                                    alt="Brand Image"></a></li>
-                        <li><a href="#"><img src="{{ asset('assets/images/partners/part4.png') }}"
-                                    alt="Brand Image"></a></li>
-                        <li><a href="#"><img src="{{ asset('assets/images/partners/part5.png') }}"
-                                    alt="Brand Image"></a></li>
-                    </ul>
-                    <ul class="brand-list brand-style-3 justify-content-center justify-content-lg-between">
-                        <li><a href="#"><img src="{{ asset('assets/images/partners/part6.png') }}"
-                                    alt="Brand Image"></a></li>
-                        <li><a href="#"><img src="{{ asset('assets/images/partners/part7.png') }}"
-                                    alt="Brand Image"></a></li>
-                        <li><a href="#"><img src="{{ asset('assets/images/partners/part8.png') }}"
-                                    alt="Brand Image"></a></li>
-                        <li><a href="#"><img src="{{ asset('assets/images/partners/part9.png') }}"
-                                    alt="Brand Image"></a></li>
-                        <li><a href="#"><img src="{{ asset('assets/images/partners/part10.png') }}"
-                                    alt="Brand Image"></a></li>
-                    </ul>
                 </div>
             </div>
         </div>
-    </div>
-    <!-- End Brand Area  -->
-    <div class="rbt-separator-mid">
-        <div class="container">
-            <hr class="rbt-separator m-0">
+        <!-- End Brand Area  -->
+        <div class="rbt-separator-mid">
+            <div class="container">
+                <hr class="rbt-separator m-0">
+            </div>
         </div>
-    </div>
-@stop
+    @stop
