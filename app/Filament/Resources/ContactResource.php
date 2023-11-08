8653e7cddef2405dac8ef0b7faabbb6da2ddb936
roombadra <coulali02@gmail.com>
2023-11-08 01:59:52 +0100

    modify

--- app/Filament/Resources/ContactResource.php
+++ app/Filament/Resources/ContactResource.php
@@ -7,13 +7,18 @@ use Filament\Tables;
 use App\Models\Contact;
 use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Tables\Actions\Action;
 use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
+use Illuminate\Support\Facades\Mail;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\RichEditor;
 use Illuminate\Database\Eloquent\Builder;
 use Filament\Infolists\Components\TextEntry;
 use App\Filament\Resources\ContactResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\ContactResource\RelationManagers;
+use App\Mail\abclub\ContactToUser;
 
 class ContactResource extends Resource
 {
@@ -57,6 +62,18 @@ class ContactResource extends Resource
                 //
             ])
             ->actions([
+                Action::make('sendEmail')
+                    ->label('Repondre au message')
+                    ->icon('heroicon-o-envelope')
+                    ->modalHeading('Envoi de mail')
+                    ->form([
+                        TextInput::make('subject')->required(),
+                        RichEditor::make('body')->required(),
+                    ])
+                    ->action(function (array $data, Contact $contact) {
+                        Mail::to($contact->email)->send(new ContactToUser($data['subject'], $data['body']));
+                        return redirect()->back()->with('success', 'Message envoyé');
+                    }),
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\DeleteAction::make(),
             ])
--- app/Filament/Resources/ContactResource/Pages/ListContacts.php
+++ app/Filament/Resources/ContactResource/Pages/ListContacts.php
@@ -14,32 +14,4 @@ use App\Models\Contact;
 class ListContacts extends ListRecords
 {
     protected static string $resource = ContactResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Action::make('approuve')
-                ->label('ENvoyer un mail')
-                ->icon('heroicon-o-envelope')
-                ->modalHeading('Creation de mail')
-                ->form([
-                    RichEditor::make('message')
-                        ->label('Contenu')
-                        ->required(),
-                ])->action(function (array $data): void {
-                    $subscribers = Contact::select('email')->get();;
-                    if ($subscribers->count() > 0) {
-                        foreach ($subscribers as $subscriber) {
-                            //Mail::to($subscriber->email)->send(new NewsletterSubscriberNotification($data['message']));
-                        }
-                    } else {
-                        Notification::make()->info()->title('Aucun abonné')->body('Aucun abonné pour le moment')->send();
-                        return;
-                    }
-
-                    Notification::make()->success()->title('Mail envoyé')->body('Le mail a été envoyé avec succès')->send();
-                    return;
-                }),
-        ];
-    }
 }
