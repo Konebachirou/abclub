32ad6cb7dd41e462b07c2c2c00853f6de34d7322
konebachirou <konebachirou1@gmail.com>
2024-01-29 15:31:56 +0100

    pull with prod

--- app/Http/Controllers/payementController.php
+++ app/Http/Controllers/payementController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Stripe\Event;
+use Stripe\PaymentIntent;
 
 class payementController extends Controller
 {
@@ -18,8 +20,7 @@ class payementController extends Controller
                             'product_data' => [
                                 'name' => 'Frais adhésion African Business Club',
                                 'images' => [
-                                    // "https://www.africanbusinessclub.org/storage/report/01HM040NXMT634DE0QCGBTZ6T4.png",
-                                    "http://127.0.0.1:8000/storage/report/01HM040NXMT634DE0QCGBTZ6T4.png"
+                                    "https://www.africanbusinessclub.org/storage/partenaire/pay.png"
                                 ]
                             ],
                         'unit_amount' => 4000 ,
@@ -38,7 +39,39 @@ class payementController extends Controller
     public function payerSucces(Request $request)
     {
          // Traitez les informations du paiement réussi ici
-         $paymentIntent = $request->input('data.object');
-         dd($paymentIntent);
+       //$event = Event::constructFrom($request->all());
+		//dd($request->session_id,$event);
+        // Vérifiez que c'est un événement de paiement réussi
+        //if ($event->type === 'payment_intent.succeeded') {
+        //    $paymentIntent = $event->data->object;
+         //	dd($paymentIntent);
+       
+      
+       	$payload = $request->getContent();
+        $sigHeader = $request->header('Stripe-Signature');
+        $endpointSecret = 'whsec_cYZuWAzPgHIjcmbEAe6Z1VgzutbXInoa'; // Remplacez par votre clé secrète de webhook
+		dd($payload, $sigHeader);
+        $event = null;
+
+        try {
+            $event = \Stripe\Webhook::constructEvent(
+                $payload, $sigHeader, $endpointSecret
+            );
+        } catch (\UnexpectedValueException $e) {
+            // Signature incorrecte
+            return response()->json(['error' => 'Signature incorrecte 1'], 400);
+        } catch (\Stripe\Exception\SignatureVerificationException $e) {
+            // Signature incorrecte
+            return response()->json(['error' => 'Signature incorrecte 2', 'desc'=> $e], 400);
+        }
+		dd($event);
+        // Traitement spécifique à l'événement
+        if ($event->type == 'payment_intent.succeeded') {
+            $paymentIntent = $event->data->object; // Contient les informations sur le paiement réussi
+            // Ajoutez ici votre logique pour traiter le paiement réussi
+        }
+
+        return response()->json(['status' => 'Webhook reçu']);
     }
+      
 }
